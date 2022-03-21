7bbdaf947e90ff1147a2aa5a9a677679f1001ac9
Danil Klimov <klymovd@gmail.com>
2022-03-21 19:40:05 +0200

    first commit

---
+++ DynamicSegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicSegmentedControl
+//
+//  Created by Danylo Klymov on 18.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
